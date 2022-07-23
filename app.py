72e72d3c68f3f76ff5cc256f3a2097d1788a1b8c
Md Noor <75603153+NoorXd786@users.noreply.github.com>
2022-07-23 20:25:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-from heroku import NAME,IMAGE, SHORT_BIO, ABOUT, GITHUB, FACEBOOK, INSTAGRAM
+from heroku import NAME,IMAGE, SHORT_BIO, ABOUT, GITHUB, SNAPCHAT, TELEGRAM, INSTAGRAM
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name=NAME, image=IMAGE, bio=SHORT_BIO, about=ABOUT, git=GITHUB, fb=FACEBOOK, ig=INSTAGRAM)
+    return render_template("index.html", name=NAME, image=IMAGE, bio=SHORT_BIO, about=ABOUT, git=GITHUB, sc=SNAPCHAT, tg=TELEGRAM, ig=INSTAGRAM)
 
 
 
